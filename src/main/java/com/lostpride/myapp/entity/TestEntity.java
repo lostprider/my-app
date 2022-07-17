c956de4923dd6db9e525a8f79547b034adf467fc
lostprider <albert.psahun@gmail.com>
2022-07-17 17:13:45 +0200

    nom del projecte: my-app, descripcíó versio final projecte my-app

---
+++ src/main/java/com/lostpride/myapp/entity/TestEntity.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.lostpride.myapp.entity;
+
+/**
+ * @author lostriver
+ *
+ */
+public class TestEntity {
+	
+	/**
+	 * Propietat de prova per la mostra de compilacio del arxiu TestEntity.
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+}
